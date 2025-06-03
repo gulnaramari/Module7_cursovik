5180a8bced2786246e45450e62ebba2c187765a5
Daniel <gulnarikmihailova@gmail.com>
2025-06-04 00:01:36 +0300

    шаблонизация

--- myproject/messenger/views.py
+++ myproject/messenger/views.py
@@ -11,86 +11,84 @@ from .forms import RecipientForm, MessageForm, MailingForm
 
 class RecipientListView(generic.ListView):
     model = Recipient
-    template_name = 'list_recipient.html'
+    template_name = 'messenger/list_recipient.html'
     context_object_name = 'recipients'
 
 
 class RecipientCreateView(generic.CreateView):
     model = Recipient
     form_class = RecipientForm
-    template_name = 'recipient_form.html'
-    success_url = reverse_lazy('list_recipient')
+    template_name = 'messenger/recipient_form.html'
+    success_url = reverse_lazy('messenger:list_recipient')
 
 
 class RecipientUpdateView(generic.UpdateView):
     model = Recipient
     form_class = RecipientForm
-    template_name = 'recipient_form.html'
-    success_url = reverse_lazy('list_recipient')
+    template_name = 'messenger/recipient_form.html'
+    success_url = reverse_lazy('messenger:list_recipient')
 
 
 class RecipientDeleteView(generic.DeleteView):
     model = Recipient
-    template_name = 'recipient_confirm_delete.html'
-    success_url = reverse_lazy('list_recipient')
+    template_name = 'messenger/recipient_confirm_delete.html'
+    success_url = reverse_lazy('messenger:list_recipient')
 
-
-# CRUD для сообщений (Message)
 
 class MessageListView(generic.ListView):
     model = Message
-    template_name = 'message_list.html'
+    template_name = 'messenger/message_list.html'
     context_object_name = 'messages'
 
 
 class MessageCreateView(generic.CreateView):
     model = Message
     form_class = MessageForm
-    template_name = 'message_form.html'
-    success_url = reverse_lazy('message_list')
+    template_name = 'messenger/message_form.html'
+    success_url = reverse_lazy('messenger:message_list')
 
 
 class MessageUpdateView(generic.UpdateView):
     model = Message
     form_class = MessageForm
-    template_name = 'message_form.html'
-    success_url = reverse_lazy('message_list')
+    template_name = 'messenger/message_form.html'
+    success_url = reverse_lazy('messenger:message_list')
 
 
 class MessageDeleteView(generic.DeleteView):
     model = Message
-    template_name = 'message_confirm_delete.html'
-    success_url = reverse_lazy('message_list')
+    template_name = 'messenger/message_confirm_delete.html'
+    success_url = reverse_lazy('messenger:message_list')
 
 
 class MailingListView(generic.ListView):
     model = Mailing
-    template_name = 'list_mailing.html'
+    template_name = 'messenger/list_mailing.html'
     context_object_name = 'mailings'
 
 
 class MailingCreateView(generic.CreateView):
     model = Mailing
     form_class = MailingForm
-    template_name = 'mailing_form.html'
-    success_url = reverse_lazy('list_mailing')
+    template_name = 'messenger/mailing_form.html'
+    success_url = reverse_lazy('messenger:list_mailing')
 
 
 class MailingUpdateView(generic.UpdateView):
     model = Mailing
     form_class = MailingForm
-    template_name = 'mailing_form.html'
-    success_url = reverse_lazy('list_mailing')
+    template_name = 'messenger/mailing_form.html'
+    success_url = reverse_lazy('messenger:list_mailing')
 
 
 class MailingDeleteView(generic.DeleteView):
     model = Mailing
-    template_name = 'mailing_confirm_delete.html'
-    success_url = reverse_lazy('list_mailing')
+    template_name = 'messenger/mailing_confirm_delete.html'
+    success_url = reverse_lazy('messenger:list_mailing')
 
 
 class HomeView(generic.TemplateView):
-    template_name = 'home.html'
+    template_name = 'messenger/home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -7,6 +7,6 @@ app_name = "users"
 urlpatterns = [
     path("register/", RegisterView.as_view(template_name='users/register.html'), name="register"),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='catalog:home'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='messenger:home'), name='logout'),
 
 ]
