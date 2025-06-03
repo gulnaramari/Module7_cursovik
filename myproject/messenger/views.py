9969c5651fdfb2521e740b42a693e999f82f4036
Daniel <gulnarikmihailova@gmail.com>
2025-06-03 14:50:27 +0300

    контроллеры и модели

--- myproject/messenger/views.py
+++ myproject/messenger/views.py
@@ -1,3 +1,71 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+from django.core.mail import send_mail
+from django.utils import timezone
+from .models import Recipient, Message, Mailing, SendAttempt
+from .forms import RecipientForm, MessageForm, MailingForm
 
-# Create your views here.
+
+# CRUD для получателей (Recipient)
+
+class RecipientListView(generic.ListView):
+    model = Recipient
+    template_name = 'recipient_list.html'
+    context_object_name = 'recipients'
+
+
+class RecipientCreateView(generic.CreateView):
+    model = Recipient
+    form_class = RecipientForm
+    template_name = 'recipient_form.html'
+    success_url = reverse_lazy('recipient_list')
+
+
+class RecipientUpdateView(generic.UpdateView):
+    model = Recipient
+    form_class = RecipientForm
+    template_name = 'recipient_form.html'
+    success_url = reverse_lazy('recipient_list')
+
+
+class RecipientDeleteView(generic.DeleteView):
+    model = Recipient
+    template_name = 'recipient_confirm_delete.html'
+    success_url = reverse_lazy('recipient_list')
+
+
+# CRUD для сообщений (Message)
+
+class MessageListView(generic.ListView):
+    model = Message
+    template_name = 'message_list.html'
+    context_object_name = 'messages'
+
+
+class MessageCreateView(generic.CreateView):
+    model = Message
+    form_class = MessageForm
+    template_name = 'message_form.html'
+    success_url = reverse_lazy('message_list')
+
+
+class MessageUpdateView(generic.UpdateView):
+    model = Message
+    form_class = MessageForm
+    template_name = 'message_form.html'
+    success_url = reverse_lazy('message_list')
+
+
+class MessageDeleteView(generic.DeleteView):
+    model = Message
+    template_name = 'message_confirm_delete.html'
+    success_url = reverse_lazy('message_list')
+
+
+# CRUD для рассылок (Mailing)
+
+class MailingListView(generic.ListView):
+    model = Mailing
+    template_name = 'mailing_list.html'
+    context_object_name = 'mailings'
