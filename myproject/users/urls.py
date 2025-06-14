39503408cd8f93b3022b87223d0f16be89704257
Daniel <gulnarikmihailova@gmail.com>
2025-06-14 10:56:46 +0300

    шаблонизация приложения users

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -9,15 +9,15 @@ urlpatterns = [
     path("register/", RegisterView.as_view(template_name='users/register.html'), name="register"),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='messenger:home'), name='logout'),
-    path('users/verify/<str:token>/', VerifyEmailView.as_view(), name='verify-email'),
+    path('users/verify/<str:token>/', VerifyEmailView.as_view(), name='verify_email'),
     path('password_reset/', auth_views.PasswordResetView.as_view(
              form_class=CustomPasswordResetForm,
-             template_name='users/registration/password_reset.html',
-             success_url=reverse_lazy('users:password_reset_done')
+             template_name='users/registration/password_recovery.html',
+             success_url=reverse_lazy('users:password_reset_form')
          ),
          name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
-             template_name='users/registration/password_reset_done.html'
+             template_name='users/registration/password_reset_form.html'
          ),
          name='password_reset_done'),
     path('reset/<uidb64>/<token>/',
@@ -26,8 +26,8 @@ urlpatterns = [
          ),
          name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-             template_name='users/registration/password_reset_complete.html'
+             template_name='users/registration/password_new.html'
          ),
-         name='password_reset_complete'),
+         name='password_new'),
 ]
 
